84a0d3026543d1379a1b98a9d0cf22dbb9d86f88
XclusivePasasin <elizar.rebollo23@itca.edu.sv>
2024-11-21 09:40:21 -0600

    Add a new variables

--- config.py
+++ config.py
@@ -7,3 +7,9 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", None)
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS", "False").lower() in ["true", "1", "t"]
+    MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")
+    MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME", "clobitechnologiesdevelopmentse@gmail.com")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", "vhok gjmx gfge inab")
+    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True").lower() in ["true", "1", "t"]
+    SALT_SECRET = os.getenv('SALT_SECRET') 
