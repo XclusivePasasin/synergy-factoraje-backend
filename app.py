b653a8ccf353864036eda6c193591cee0f6a6a11
XclusivePasasin <elizar.rebollo23@itca.edu.sv>
2024-11-24 22:59:43 -0600

    Add new route

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from seeds.seeds import init_app
 from routes.email_route import email_bp  
 from routes.facturas_route import facturas_bp
 from routes.usuario_route import usuarios_bp
-
+from routes.solicitudes_route import solicitud_bp
 
 def create_app():
     app = Flask(__name__)
@@ -34,6 +34,7 @@ def create_app():
     app.register_blueprint(email_bp, url_prefix='/api/email')
     app.register_blueprint(facturas_bp, url_prefix='/api/factura')
     app.register_blueprint(usuarios_bp, url_prefix='/api/usuario')
+    app.register_blueprint(solicitud_bp, url_prefix='/api/solicitud')
 
     return app
 
